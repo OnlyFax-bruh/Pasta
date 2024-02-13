2fa21540836ffc8af75d245fbfd171c205c34a99
AC1111111 <83080295+AC1111111@users.noreply.github.com>
2024-02-13 19:46:22 +0530

    Let them eat Pasta

---
+++ commands/utility/createPasta.js
@@ -0,0 +1,59 @@
+require("dotenv").config();
+const {
+	MongoClient,
+	ServerApiVersion,
+} = require("mongodb");
+
+const { EmbedBuilder } = require("discord.js");
+const { SlashCommandBuilder } = require("discord.js");
+
+mongoPassword = process.env.MONGO_PASSWORD;
+const uri = `mongodb+srv://adminpastabot:${mongoPassword}@clusterpasta.ketfdz1.mongodb.net/?retryWrites=true&w=majority`;
+
+const mongoClient = new MongoClient(uri, {
+	serverApi: {
+		version: ServerApiVersion.v1,
+		strict: true,
+		deprecationErrors: true,
+	},
+});
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName("makepasta")
+		.setDescription(
+			"Birth an abomination for pasta to spam. This is the joy of creation."
+		)
+		
+		.addStringOption((option) =>
+			option
+				.setName("title")
+				.setDescription("Name dat shit")
+				.setRequired(true)
+		)
+
+		.addStringOption((option) =>
+			option
+				.setName("pasta")
+				.setDescription("This pasta is a monster")
+				.setRequired(true)
+		),
+
+
+	async execute(interaction) {
+		// Connect to PastaDB within MongoDB
+		const pastaDB = mongoClient.db("PastaDB");
+		const copyPastaCollection = pastaDB.collection(
+			"CopyPastas"
+		);
+		
+		//Simple document insertion
+		const doc = {
+			title: interaction.options.getString("title"),
+			body: interaction.options.getString("pasta")
+		};
+		await copyPastaCollection.insertOne(doc);
+
+		await interaction.reply(`New Pasta "${doc.title}" up and ready to go`)
+	},
+};
