16d616fb0308f741b9fd656292e479d17a1f9512
AC1111111 <83080295+AC1111111@users.noreply.github.com>
2024-02-14 18:10:06 +0530

    I'll do this later

--- commands/utility/pfp.js
+++ commands/utility/pfp.js
@@ -3,7 +3,7 @@
 const { EmbedBuilder } = require("discord.js");
 const { SlashCommandBuilder } = require("discord.js");
 
-module.exports = {
+/*module.exports = {
 	data: new SlashCommandBuilder()
 		.setName("getpfp")
 		.setDescription("Get any member's pfp")
@@ -16,18 +16,18 @@ module.exports = {
 				)
 				.setRequired(true)
 		)
-		/*.addBooleanOption((option) =>
+		.addBooleanOption((option) =>
 			option
 				.setName("serverpfp")
 				.setDescription(
 					"Yoink the server avatar or personal avatar?"
 				)
 				.setRequired(true)
-		)*/,
+		),
 	async execute(interaction) {
 		
 		const target = interaction.options.getUser("target") ?? ""; //Why do I need to do this last part again?
-		/*const guild = interaction.member.guild;
+		const guild = interaction.member.guild;
 		const guildTarget = ;
 		var finalTarget;
 		if(interaction.options.getBoolean("serverpfp")){
@@ -35,7 +35,7 @@ module.exports = {
 		}
 		else{
 			finalTarget = target;
-		}*/
+		}
 
 
 		const embed = new EmbedBuilder()
@@ -49,4 +49,22 @@ module.exports = {
 			);
 		await interaction.reply({ embeds: [embed] });
 	},
+};*/
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('getpfp')
+        .setDescription("Get any member's pfp")
+        .addUserOption(option =>
+            option
+                .setName('target')
+                .setDescription('Nice pfp, would be a shame if I yoinked it')
+                .setRequired(true)),
+    async execute(interaction) {
+        const target = interaction.options.getUser('target') ?? '';
+        const embed = new EmbedBuilder()
+            .setTitle(`${target.username}'s pfp`)
+            .setImage(target.displayAvatarURL({ format: 'png', size: 2048, dynamic: true }));
+        await interaction.reply({embeds: [embed] });
+    },
 };
