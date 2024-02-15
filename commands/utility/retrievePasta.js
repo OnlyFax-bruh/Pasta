03ef9c5b3e0f279193213d8d4cebe84e411ae31b
AC1111111 <83080295+AC1111111@users.noreply.github.com>
2024-02-15 15:11:20 +0530

    Get pastas from the DB

---
+++ commands/utility/retrievePasta.js
@@ -0,0 +1,47 @@
+require("dotenv").config();
+const {
+	MongoClient,
+	ServerApiVersion,
+} = require("mongodb");
+
+const { EmbedBuilder } = require("discord.js");
+const { SlashCommandBuilder } = require("discord.js");
+
+mongoPassword = process.env.MONGO_PASSWORD;
+const uri = `mongodb+srv://adminpastabot:${mongoPassword}@clusterpasta.ketfdz1.mongodb.net/?retryWrites=true&w=majority`;
+
+const mongoClient = new MongoClient(uri, {
+	serverApi: {
+		version: ServerApiVersion.v1,
+		strict: true,
+		deprecationErrors: true,
+	},
+});
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName("makepasta")
+		.setDescription(
+			"Make pasta spit your brainrot"
+		)
+
+		.addStringOption((option) =>
+			option
+				.setName("title")
+				.setDescription("Pasta name")
+				.setRequired(true)
+		),
+
+	async execute(interaction) {
+		// Connect to PastaDB within MongoDB
+		const pastaDB = mongoClient.db("PastaDB");
+		const copyPastaCollection =
+			pastaDB.collection("CopyPastas");
+
+		const doc = copyPastaCollection.findOne({
+            title : interaction.options.getString("title")
+        });
+
+		await interaction.reply(doc.body);
+	},
+};
