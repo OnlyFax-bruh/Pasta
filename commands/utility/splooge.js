77547e446da88c0317706a0b467101e6902be0eb
ByteZaDusto <bytezadusto@gmail.com>
2024-09-08 11:11:22 +0200

    fix splooge

--- commands/utility/splooge.js
+++ commands/utility/splooge.js
@@ -40,27 +40,29 @@ module.exports = {
 		// This returns an array even tho theres only one doc.
 		// the method for returning only one doc exists and works technically but its cringe
 
+		// Connect to PastaDB within MongoDB
 		const sploogeDocArray = await pastaCollection
-			.find({
-				documentName: "splooge",
-			})
-			.project({ jacks: 1, initDate: 1, _id: 0 })
-			.toArray();
+		.find({
+			documentName: "splooge",
+		})
+		.project({ jacks: 1, initDate: 1, _id: 0 })
+		.toArray();
 		// access the first and only element now
 		const sploogeDoc = sploogeDocArray[0];
-		// maybe turn this into const initDate = sploogeDoc.initDate so its not the current time
 		const initDate = sploogeDoc.initDate;
 		const filter = { documentName: "splooge" };
 		const newJacks = sploogeDoc.jacks + 1;
 		const updateDoc = {
-			$set: { jacks: newJacks },
+		$set: { jacks: newJacks },
 		};
 		pastaCollection.updateOne(filter, updateDoc);
-		console.log("Splooge Initdate: " + initDate);
-		await interaction.reply({
-			content: `<@806964705008025611> has jacked off ${
-				sploogeDoc.jacks - 1
-			} times since ${initDate}`,
-		});
+
+		randomIndex = Math.floor(Math.random() * possibleContent.length+1)
+
+		content = `<@806964705008025611> ${possibleContent[randomIndex]}${
+		sploogeDoc.jacks - 1
+		} times since ${initDate}`;
+		return await content;
 	},
 };
+
